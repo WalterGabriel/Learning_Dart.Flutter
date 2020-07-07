2a334f04c61a50cbcbd7d693dabde433824a2052
Walter G. Lima <waltergabrielml@gmail.com>
2020-07-07 13:45:25 +0200

    Composição_de_Classes

--- Dart Orientado a Objetos/alura_bank/bin/main.dart
+++ Dart Orientado a Objetos/alura_bank/bin/main.dart
@@ -1,37 +1,40 @@
+/* COMPOSIÇÃO DE CLASSES:
+  1 -  Uma pode utilizar outras como uma propriedade.*/
+
 void main() {
-  var contaAmanda = ContaCorrente();
-  contaAmanda.titular = 'Amanda';
-  contaAmanda.saldo = 120.00;
-  print(
-      'Saldo da conta de ${contaAmanda.titular} é de: R\$ ${contaAmanda.saldo}');
-  contaAmanda.deposito(50.00);
-  contaAmanda.saque(220);
+  var conta001 = ContaCorrente();
 
-  double sucesso = contaAmanda.deposito(1.0);
-  print(sucesso);
+  // ignore: omit_local_variable_types
+  Cliente amanda = Cliente();
+  amanda.nome = 'Amanda';
+  amanda.cpf = '098.344.890-45';
+  amanda.profissao = 'Programadora';
 
-  var contaTiago = ContaCorrente();
-  contaTiago.titular = 'Tiago';
-  contaTiago.saldo = 100.00;
-  print(
-      'Saldo da conta de ${contaTiago.titular} é de: R\$ ${contaTiago.saldo}');
-  contaTiago.deposito(50.00);
-  contaTiago.saque(220);
+  conta001.titular = amanda;
+
+  print('Titular: ${conta001.titular.nome}');
+  print('CPF: ${conta001.titular.cpf}');
+  print('Profissão: ${conta001.titular.profissao}');
+}
+
+//Criação classe cliente
+class Cliente {
+  String nome;
+  String cpf;
+  String profissao;
 }
 
 //Criação da classe conta corrente.
 class ContaCorrente {
-  String titular;
+  Cliente titular;
   int agencia;
   int conta;
   double saldo = 0.0;
-  /*CRIANDO MÉTDOS: Métodos são generalizações de funcionalidades dentro das classes que servem para facilitar a manipulação de dados dentro dos objetos uma vez que os objetos criados precisarão ter as propriedades de uma classe manipuladas dentros deles próprios.*/
+  double chequeEspecial = -100.00;
 
-  // 1 - Declara-se o tipo de uma operação, assim como se declara um tipo de variável, nesse caso, "void" uma operação sem retorno.
-
+//Método saque
   void saque(double valorDoSaque) {
     if (saldo - valorDoSaque < -100) {
-      //referência a classe que esta sendo utilizada. Nesse caso, essa mesma classe.
       print(
           'Sem saldo suficiente para realizar o saque de $valorDoSaque seu saque máximo é de ${saldo + 100}');
     } else {
@@ -42,8 +45,7 @@ class ContaCorrente {
     }
   }
 
-  // 2 -  Para operações com retorno basta inidica o tipo de retorno desejado e em seguida utilizar a função return assim que desejado.
-
+//Método deposito
   double deposito(double valorDoDeposito) {
     saldo += valorDoDeposito;
     return saldo;
