0fa249ae52040b35c3ff594dae78b7d0fb74e316
Walter G. Lima <waltergabrielml@gmail.com>
2020-07-07 10:31:25 +0200

    Métodos com retorno

--- Dart Orientado a Objetos/alura_bank/bin/main.dart
+++ Dart Orientado a Objetos/alura_bank/bin/main.dart
@@ -2,13 +2,20 @@ void main() {
   var contaAmanda = ContaCorrente();
   contaAmanda.titular = 'Amanda';
   contaAmanda.saldo = 120.00;
-  print('Saldo da ${contaAmanda.titular} é de: R\$ ${contaAmanda.saldo}');
+  print(
+      'Saldo da conta de ${contaAmanda.titular} é de: R\$ ${contaAmanda.saldo}');
+  contaAmanda.deposito(50.00);
   contaAmanda.saque(220);
+
+  double sucesso = contaAmanda.deposito(1.0);
+  print(sucesso);
 
   var contaTiago = ContaCorrente();
   contaTiago.titular = 'Tiago';
   contaTiago.saldo = 100.00;
-  print('Saldo da ${contaTiago.titular} é de: R\$ ${contaTiago.saldo}');
+  print(
+      'Saldo da conta de ${contaTiago.titular} é de: R\$ ${contaTiago.saldo}');
+  contaTiago.deposito(50.00);
   contaTiago.saque(220);
 }
 
@@ -20,17 +27,25 @@ class ContaCorrente {
   double saldo = 0.0;
   /*CRIANDO MÉTDOS: Métodos são generalizações de funcionalidades dentro das classes que servem para facilitar a manipulação de dados dentro dos objetos uma vez que os objetos criados precisarão ter as propriedades de uma classe manipuladas dentros deles próprios.*/
 
-  // 1 - Declara-se o tipo de uma operação, assim como se declara um tipo de variável, nesse caso, "void" uma operação sem retorno:
+  // 1 - Declara-se o tipo de uma operação, assim como se declara um tipo de variável, nesse caso, "void" uma operação sem retorno.
 
   void saque(double valorDoSaque) {
     if (saldo - valorDoSaque < -100) {
       //referência a classe que esta sendo utilizada. Nesse caso, essa mesma classe.
-      print('Sem saldo suficiente para realizar o saque de $valorDoSaque seu saque máximo é de ${saldo + 100}');
+      print(
+          'Sem saldo suficiente para realizar o saque de $valorDoSaque seu saque máximo é de ${saldo + 100}');
     } else {
       print('Saque de R\$ $valorDoSaque');
-      this.saldo -= valorDoSaque;
+      saldo -= valorDoSaque;
 
       print('Novo saldo da ${titular} é de: R\$ ${saldo}');
     }
+  }
+
+  // 2 -  Para operações com retorno basta inidica o tipo de retorno desejado e em seguida utilizar a função return assim que desejado.
+
+  double deposito(double valorDoDeposito) {
+    saldo += valorDoDeposito;
+    return saldo;
   }
 }
